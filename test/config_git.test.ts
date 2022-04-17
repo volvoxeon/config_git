43c29609b59b7eccb59f4233f2a18b299b1b577a
volvoxeon <yuhezhouxinyu@126.com>
2022-04-17 13:26:55 +0800

    Initial commit

---
+++ test/config_git.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ConfigGit from '../lib/config_git-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/config_git-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ConfigGit.ConfigGitStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
